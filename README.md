54c366008cec67cbb2f9fe3763589518dd0c8582
Galca Vlad-Daniel <vladgalca@gmail.com>
2021-06-13 15:23:01 +0300

    Recreated project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyMountainHikes
+# ProiectAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.5.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-mountain-hikes'),
+      dir: require('path').join(__dirname, './coverage/proiect-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
